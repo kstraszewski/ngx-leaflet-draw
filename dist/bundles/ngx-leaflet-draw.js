93853bd5552c687689d5329f92ca1fd7b3afcb20
Konrad Straszewski <koonradstraszewski@gmail.com>
2017-09-10 01:45:54 +0200

    adding output, and option to override onCreated

--- dist/bundles/ngx-leaflet-draw.js
+++ dist/bundles/ngx-leaflet-draw.js
@@ -8,6 +8,15 @@
 var LeafletDrawDirective = /** @class */ (function () {
     function LeafletDrawDirective(leafletDirective) {
         this.drawOptions = null;
+        this.onCreate = new core.EventEmitter();
+        this.onDrawStart = new core.EventEmitter();
+        this.onDrawStop = new core.EventEmitter();
+        this.onDeleted = new core.EventEmitter();
+        this.onDeleteStart = new core.EventEmitter();
+        this.onDeleteStop = new core.EventEmitter();
+        this.onEdited = new core.EventEmitter();
+        this.onEditStart = new core.EventEmitter();
+        this.onEditStop = new core.EventEmitter();
         this.leafletDirective = new ngxLeaflet.LeafletDirectiveWrapper(leafletDirective);
     }
     LeafletDrawDirective.prototype.ngOnInit = function () {
@@ -22,10 +31,76 @@ var LeafletDrawDirective = /** @class */ (function () {
         // Add the control to the map
         this.leafletDirective.getMap().addControl(this.drawControl);
         // Register the main handler for events coming from the draw plugin
-        this.leafletDirective.getMap().on(L.Draw.Event.CREATED, function (e) {
-            var layer = e.layer;
-            _this.featureGroup.addLayer(layer);
-        });
+        /*Create*/
+        if (this.onCreate.observers.length === 0) {
+            this.leafletDirective.getMap().on(L.Draw.Event.CREATED, function (e) {
+                var layer = e.layer;
+                _this.featureGroup.addLayer(layer);
+            });
+        }
+        else {
+            this.leafletDirective.getMap().on(L.Draw.Event.CREATED, function (e) {
+                _this.onCreate.emit(e);
+            });
+        }
+        if (this.onDrawStart.observers.length !== 0) {
+            this.leafletDirective
+                .getMap()
+                .on(L.Draw.Event.DRAWSTART, function (e) {
+                _this.onDrawStart.emit(e);
+            });
+        }
+        if (this.onDrawStop.observers.length !== 0) {
+            this.leafletDirective
+                .getMap()
+                .on(L.Draw.Event.DRAWSTOP, function (e) {
+                _this.onDrawStop.emit(e);
+            });
+        }
+        /*Edit*/
+        if (this.onEdited.observers.length !== 0) {
+            this.leafletDirective
+                .getMap()
+                .on(L.Draw.Event.EDITED, function (e) {
+                _this.onEdited.emit(e);
+            });
+        }
+        if (this.onEditStart.observers.length !== 0) {
+            this.leafletDirective
+                .getMap()
+                .on(L.Draw.Event.EDITSTART, function (e) {
+                _this.onEditStart.emit(e);
+            });
+        }
+        if (this.onEditStop.observers.length !== 0) {
+            this.leafletDirective
+                .getMap()
+                .on(L.Draw.Event.EDITSTOP, function (e) {
+                _this.onEditStop.emit(e);
+            });
+        }
+        /*Delete*/
+        if (this.onDeleted.observers.length !== 0) {
+            this.leafletDirective
+                .getMap()
+                .on(L.Draw.Event.DELETED, function (e) {
+                _this.onDeleted.emit(e);
+            });
+        }
+        if (this.onDeleteStart.observers.length !== 0) {
+            this.leafletDirective
+                .getMap()
+                .on(L.Draw.Event.DELETESTART, function (e) {
+                _this.onDeleteStart.emit(e);
+            });
+        }
+        if (this.onDeleteStop.observers.length !== 0) {
+            this.leafletDirective
+                .getMap()
+                .on(L.Draw.Event.DELETESTOP, function (e) {
+                _this.onDeleteStop.emit(e);
+            });
+        }
     };
     LeafletDrawDirective.prototype.ngOnDestroy = function () {
         this.leafletDirective.getMap().removeControl(this.drawControl);
@@ -63,6 +138,15 @@ var LeafletDrawDirective = /** @class */ (function () {
     ]; };
     LeafletDrawDirective.propDecorators = {
         'drawOptions': [{ type: core.Input, args: ['leafletDrawOptions',] },],
+        'onCreate': [{ type: core.Output },],
+        'onDrawStart': [{ type: core.Output },],
+        'onDrawStop': [{ type: core.Output },],
+        'onDeleted': [{ type: core.Output },],
+        'onDeleteStart': [{ type: core.Output },],
+        'onDeleteStop': [{ type: core.Output },],
+        'onEdited': [{ type: core.Output },],
+        'onEditStart': [{ type: core.Output },],
+        'onEditStop': [{ type: core.Output },],
     };
     return LeafletDrawDirective;
 }());
--- src/leaflet-draw/core/leaflet-draw.directive.ts
+++ src/leaflet-draw/core/leaflet-draw.directive.ts
@@ -1,9 +1,9 @@
-import { Directive, Input, OnChanges, OnInit, SimpleChange } from '@angular/core';
+import {Directive, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange} from '@angular/core';
 
 import * as L from 'leaflet';
 import 'leaflet-draw';
 
-import { LeafletDirective, LeafletDirectiveWrapper } from '@asymmetrik/ngx-leaflet';
+import {LeafletDirective, LeafletDirectiveWrapper} from '@asymmetrik/ngx-leaflet';
 
 
 @Directive({
@@ -19,6 +19,18 @@ export class LeafletDrawDirective
 
 	@Input('leafletDrawOptions') drawOptions: L.Control.DrawConstructorOptions = null;
 
+	@Output() onCreate: EventEmitter<L.DrawEvents.Created> = new EventEmitter<L.DrawEvents.Created>();
+	@Output() onDrawStart: EventEmitter<L.DrawEvents.DrawStart> = new EventEmitter<L.DrawEvents.DrawStart>();
+	@Output() onDrawStop: EventEmitter<L.DrawEvents.DrawStop> = new EventEmitter<L.DrawEvents.DrawStop>();
+
+	@Output() onDeleted: EventEmitter<L.DrawEvents.Deleted> = new EventEmitter<L.DrawEvents.Deleted>();
+	@Output() onDeleteStart: EventEmitter<L.DrawEvents.DeleteStart> = new EventEmitter<L.DrawEvents.DeleteStart>();
+	@Output() onDeleteStop: EventEmitter<L.DrawEvents.DeleteStop> = new EventEmitter<L.DrawEvents.DeleteStop>();
+
+	@Output() onEdited: EventEmitter<L.DrawEvents.Edited> = new EventEmitter<L.DrawEvents.Edited>();
+	@Output() onEditStart: EventEmitter<L.DrawEvents.EditStart> = new EventEmitter<L.DrawEvents.EditStart>();
+	@Output() onEditStop: EventEmitter<L.DrawEvents.EditStop> = new EventEmitter<L.DrawEvents.EditStop>();
+
 	constructor(leafletDirective: LeafletDirective) {
 		this.leafletDirective = new LeafletDirectiveWrapper(leafletDirective);
 	}
@@ -30,7 +42,7 @@ export class LeafletDrawDirective
 		this.drawOptions = this.initializeDrawOptions(this.drawOptions);
 
 		// Create the control
-		this.drawControl =  new L.Control.Draw(this.drawOptions);
+		this.drawControl = new L.Control.Draw(this.drawOptions);
 
 		// Pull out the feature group for convenience
 		this.featureGroup = this.drawOptions.edit.featureGroup;
@@ -39,10 +51,79 @@ export class LeafletDrawDirective
 		this.leafletDirective.getMap().addControl(this.drawControl);
 
 		// Register the main handler for events coming from the draw plugin
-		this.leafletDirective.getMap().on(L.Draw.Event.CREATED, (e: any) => {
-			const layer = (e as L.DrawEvents.Created).layer;
-			this.featureGroup.addLayer(layer);
-		});
+
+		/*Create*/
+		if (this.onCreate.observers.length === 0) {
+			this.leafletDirective.getMap().on(L.Draw.Event.CREATED, (e: any) => {
+				const layer = (e as L.DrawEvents.Created).layer;
+				this.featureGroup.addLayer(layer);
+			});
+		}
+		else {
+			this.leafletDirective.getMap().on(L.Draw.Event.CREATED, (e: any) => {
+				this.onCreate.emit(e);
+			});
+		}
+		if (this.onDrawStart.observers.length !== 0) {
+			this.leafletDirective
+				.getMap()
+				.on(L.Draw.Event.DRAWSTART, (e: any) => {
+					this.onDrawStart.emit(e);
+				});
+		}
+		if (this.onDrawStop.observers.length !== 0) {
+			this.leafletDirective
+				.getMap()
+				.on(L.Draw.Event.DRAWSTOP, (e: any) => {
+					this.onDrawStop.emit(e);
+				});
+		}
+
+		/*Edit*/
+		if (this.onEdited.observers.length !== 0) {
+			this.leafletDirective
+				.getMap()
+				.on(L.Draw.Event.EDITED, (e: any) => {
+					this.onEdited.emit(e);
+				});
+		}
+		if (this.onEditStart.observers.length !== 0) {
+			this.leafletDirective
+				.getMap()
+				.on(L.Draw.Event.EDITSTART, (e: any) => {
+					this.onEditStart.emit(e);
+				});
+		}
+		if (this.onEditStop.observers.length !== 0) {
+			this.leafletDirective
+				.getMap()
+				.on(L.Draw.Event.EDITSTOP, (e: any) => {
+					this.onEditStop.emit(e);
+				});
+		}
+
+		/*Delete*/
+		if (this.onDeleted.observers.length !== 0) {
+			this.leafletDirective
+				.getMap()
+				.on(L.Draw.Event.DELETED, (e: any) => {
+					this.onDeleted.emit(e);
+				});
+		}
+		if (this.onDeleteStart.observers.length !== 0) {
+			this.leafletDirective
+				.getMap()
+				.on(L.Draw.Event.DELETESTART, (e: any) => {
+					this.onDeleteStart.emit(e);
+				});
+		}
+		if (this.onDeleteStop.observers.length !== 0) {
+			this.leafletDirective
+				.getMap()
+				.on(L.Draw.Event.DELETESTOP, (e: any) => {
+					this.onDeleteStop.emit(e);
+				});
+		}
 	}
 
 	ngOnDestroy() {
